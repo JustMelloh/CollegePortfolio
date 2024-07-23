c313917900a9ad5a5d09f389a2febff6bc193b12
Austin <Austin.reid@keyin.com>
2024-07-23 10:10:35 -0230

    Finished Assignment

---
+++ S4Classes/SDAT/Queues/Main.java
@@ -0,0 +1,13 @@
+package Queues;
+
+public class Main {
+    public static void main(String[] args) {
+        QueueArray queue = new QueueArray(5);
+
+    }
+
+    /* An Animal Shelter, which holds only Dogs and cats, operates on a strictly first in first out basis
+    * people must adopt either the oldest (based on arrival) of animals at the shelter, or they can select whether they would prefer
+    * a dog or a cat they cannot select which specific animal they would like, create the data structures to maintain this system and implement operations such as
+    * enqueue, dequeueAny, dequeueDog, dequeue Cat*/
+}
